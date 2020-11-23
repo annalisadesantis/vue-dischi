7eb183600b12acb84bfcc2e398e676d6ed2e1755
Annalisa <annalisadesantis770@gmail.com>
2020-11-23 14:29:19 +0100

    stampa in pagina + css

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
+// L'API da chiamare è: https://flynn.boolean.careers/exercises/api/array/music
+// BONUS 1: Creare una select con tutti i generi dei dischi. In base al genere che viene selezionato nella select, vengono visualizzati i cd corrispondenti.
+// N.B.: per fare questo punto non è necessario modificare l'array dei dati
+// BONUS 2: Ordinare i dischi per anno di uscita.
+
 var app = new Vue({
     el: "#root",
     data: {
-
+        dischi: [],
+        arraydischi: 10
     },
     mounted() {
-            axios
-            .get('')
-            .then(function(response) {
 
+        axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) => {
+                this.dischi = response.data.response;
+                console.log(this.dischi);
             });
-        }
-
-
 
     }
 
